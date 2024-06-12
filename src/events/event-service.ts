599bea6cebf7fdec28fb691741f0a568d8946ed6
JustTrott <greenvisp@gmail.com>
2024-06-12 08:48:52 +0500

    bronze level

--- src/events/event-service.ts
+++ src/events/event-service.ts
@@ -1,37 +1,37 @@
-import mongoose from 'mongoose';
-import { CreateEventDto } from './dtos/CreateEvent.dot';
-import EventModel, { IEvent } from './models/Event';
-import { Event } from './types/response';
-
-
+import mongoose from "mongoose";
+import { CreateEventDto } from "./dtos/CreateEvent.dot";
+import EventModel, { IEvent } from "./models/Event";
+import { Event } from "./types/response";
 
 // this event service instance shows how to create a event, get a event by id, and get all events with in-memory data
 class EventService {
-  
-    async getEventById(id: string): Promise<IEvent | null> {
-      return await EventModel.findById(id).exec();
-    }
+	async getEventById(id: string): Promise<IEvent | null> {
+		return await EventModel.findOne({ id });
+	}
 
-    async getEvents(): Promise<IEvent[]> {
-      return await EventModel.find().exec(); 
-    }
+	async getEvents(): Promise<IEvent[]> {
+		return await EventModel.find().exec();
+	}
 
-    async createEvent(createEventDto: CreateEventDto): Promise<IEvent> {
-      const { name, description, date, location ,duration} = createEventDto;
-      const newEvent = new EventModel({
-        name,
-        description,
-        date: new Date(date),
-        location,
-        duration
-      });
-  
-      await newEvent.save();
-      return newEvent;
-    }
-  
-    
-  }
-  
-  export default EventService;
-  
+	async createEvent(createEventDto: CreateEventDto): Promise<IEvent> {
+		const { name, description, date, location, duration } = createEventDto;
+		const latestEvent = await EventModel.findOne()
+			.sort({ id: -1 }) // Sort by ID in descending order
+			.exec();
+
+		const nextId = latestEvent ? latestEvent.id + 1 : 1; // Start with 1 if no events exist
+		const newEvent = new EventModel({
+			id: nextId,
+			name,
+			description,
+			date: new Date(date),
+			location,
+			duration,
+		});
+
+		await newEvent.save();
+		return newEvent;
+	}
+}
+
+export default EventService;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import 'dotenv/config';
-import express from 'express';
-import connectDB from './db';
-import globalRouter from './global-router';
-import { logger } from './logger';
+import "dotenv/config";
+import express from "express";
+import connectDB from "./db";
+import globalRouter from "./global-router";
+import { logger } from "./logger";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,13 +11,12 @@ connectDB();
 
 app.use(logger);
 app.use(express.json());
-app.use('/api/v1/',globalRouter);
+app.use("/api/v1/", globalRouter);
 
-
-app.get('/helloworld',(request,response) =>{
-  response.send("Hello World!");
-})
+app.get("/", (request, response) => {
+	response.send("Please go to /api/v1/events");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server runs at http://localhost:${PORT}`);
+	console.log(`Server runs at http://localhost:${PORT}`);
 });
